d4d9a0147648f5cb4f49cd9e62e24183ca705b14
affu-1205 <124686245+affu-1205@users.noreply.github.com>
2023-03-03 15:20:16 +0530

    Delete java/com/example/com directory

--- java/com/example/com/repo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.com.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.com.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-
-}
